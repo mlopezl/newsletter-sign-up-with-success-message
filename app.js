2269015e16b8ffeb6ffc4e67af5c6ca1cf3d19ed
Maynor Lopez <maynor94l39@gmail.com>
2026-03-01 15:11:31 -0600

    Adding form validation and active states

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
-const newsletter = document.querySelector('.newsletter');
-const newsletterSuccessMessage = document.querySelector('.newsletter__success-message');
-const newsletterSubmitButton = document.querySelector('.newsletter__submit-button');
+const newsletter = document.querySelector(".newsletter");
+const newsletterSuccessMessage = document.querySelector(
+  ".newsletter__success-message",
+);
+const newsletterSubmitButton = document.querySelector(
+  ".newsletter__submit-button",
+);
+const emailInput = document.getElementById("email-input");
+const emailErrorMessage = document.querySelector(".email__error-message");
 
-newsletterSubmitButton.addEventListener('click', (event) => {
+newsletterSubmitButton.addEventListener("click", (event) => {
   event.preventDefault();
-  newsletter.classList.add('hide');
-  newsletterSuccessMessage.classList.remove('hide');
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  if (emailRegex.test(emailInput.value)) {
+    FormSubmitSuccess();
+  } else {
+    showFormSubmitError();
+  }
 });
+
+emailInput.addEventListener("input", () => {
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  if (emailRegex.test(emailInput.value)) {
+    deleteFormSubmitError();
+  } else {
+    showFormSubmitError();
+  }
+});
+
+function FormSubmitSuccess() {
+  newsletter.classList.add("hide");
+  newsletterSuccessMessage.classList.remove("hide");
+}
+
+function showFormSubmitError() {
+  emailInput.classList.add("email__error");
+  emailErrorMessage.classList.remove("hide");
+}
+
+function deleteFormSubmitError() {
+  emailInput.classList.remove("email__error");
+  emailErrorMessage.classList.add("hide");
+}
